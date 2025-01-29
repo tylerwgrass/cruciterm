d8af9be3e330307d4b0c9a94e398275135e245d9
Tyler Grass <twgrass12@gmail.com>
2025-01-29 10:54:16 -0800

    add file loading via command line

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/tylerwgrass/cruciterm/loader"
 	"github.com/tylerwgrass/cruciterm/puzzle"
 )
 
+var TEST_FILE_PATH string = "./puzzles/test.puz"
+
 func main() {
-	puzFile := "./puzzles/test.puz"
-	puz, err := loader.LoadFile(puzFile)
+	puzFilePath := TEST_FILE_PATH
+	if len(os.Args) == 2 {
+		puzFilePath = os.Args[1]
+	}
+
+	puz, err := loader.LoadFile(puzFilePath)
 	if err != nil {
 		fmt.Println(err)
 		return
