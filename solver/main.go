99c853cf6d8cc7a33b72d971175f5e74c8e3e287
Tyler Grass <twgrass12@gmail.com>
2025-01-30 12:39:58 -0800

    add solution validation

--- solver/main.go
+++ solver/main.go
@@ -18,6 +18,7 @@ type mainModel struct {
 	downClues map[int]string
 	clues cluesModel
 	grid tea.Model
+	solution string
 	cursorX int
 	cursorY int
 }
@@ -44,6 +45,7 @@ func initMainModel(puz *puzzle.PuzzleDefinition) mainModel {
 		downClues: puz.DownClues,
 		grid: grid,
 		clues: clues,
+		solution: puz.Answer,
 		cursorX: initialX,
 		cursorY: initialY,
 	}
@@ -62,12 +64,14 @@ func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 	m.grid, _ = m.grid.Update(msg)
-
 	return m, nil
 }
 
 func (m mainModel) View() string {
 	header := fmt.Sprintf("%s\n%s %s\n", m.title, m.author, m.copyright)
+	if m.validateSolution() {
+		header += "Solved!\n"
+	}
 	footer := ("\nPress ctrl+c to quit.\n")
 	return lipgloss.JoinVertical(
 		lipgloss.Center,
@@ -77,6 +81,19 @@ func (m mainModel) View() string {
 	)
 }
 
+	func (m mainModel) validateSolution() bool {
+		grid := m.grid.(gridModel).Grid
+		numRows := len(grid)
+		numCols := len(grid[0])
+		for i := 0; i < numRows; i++ {
+			for j := 0; j < numCols; j++ {
+				if (grid[i][j] != string(m.solution[(i*numCols)+j])) {
+					return false	
+				}
+			}
+		}
+		return true 
+	}
 
 func Run(puz *puzzle.PuzzleDefinition) {
 	p := tea.NewProgram(initMainModel(puz))
