39eb6d9ea221be06daadc4829eecdef230874683
Tyler Grass <twgrass12@gmail.com>
2025-03-18 13:37:09 -0700

    use alt screen and center content

--- go.mod
+++ go.mod
@@ -23,5 +23,6 @@ require (
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/term v0.30.0 // indirect
 )
--- solver/main.go
+++ solver/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/charmbracelet/lipgloss"
 	"github.com/tylerwgrass/cruciterm/logger"
 	"github.com/tylerwgrass/cruciterm/puzzle"
+	"golang.org/x/term"
 )
 
 type mainModel struct {
@@ -49,17 +50,24 @@ func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m mainModel) View() string {
+	width, height, err := term.GetSize(0)
+	if err != nil {
+		logger.Debug(fmt.Sprintf("Failed to get terminal size"))
+		width = 500
+		height = 500
+	}
 	header := fmt.Sprintf("%s\n%s %s\n", m.title, m.author, m.copyright)
 	if m.grid.(gridModel).solved {
 		header += "Solved!\n"
 	}
 	footer := ("\nPress ctrl+c to quit.\n")
-	return lipgloss.JoinVertical(
+	mainContent := lipgloss.JoinVertical(
 		lipgloss.Center,
 		header,
-		lipgloss.JoinHorizontal(lipgloss.Top, m.grid.View(), m.clues.View()),
+		lipgloss.NewStyle().AlignVertical(lipgloss.Center).Render(lipgloss.JoinHorizontal(lipgloss.Top, m.grid.View(), m.clues.View())),
 		footer,
 	)
+	return lipgloss.Place(width, height, lipgloss.Center, lipgloss.Center, mainContent)
 }
 
 func Run(puz *puzzle.PuzzleDefinition) {
@@ -72,7 +80,7 @@ func Run(puz *puzzle.PuzzleDefinition) {
 	logger.SetLogFile(f)
 	defer f.Close()
 
-	p := tea.NewProgram(initMainModel(puz))
+	p := tea.NewProgram(initMainModel(puz), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 			fmt.Printf("Alas, there's been an error: %v", err)
 			os.Exit(1)
